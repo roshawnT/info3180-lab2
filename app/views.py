a170a3e0cadf2809c14533edc5506565566ea72c
Ubuntu <ubuntu@ip-172-31-38-24.ec2.internal>
2020-02-08 02:16:29 +0000

    Profile

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import datetime 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -52,6 +52,17 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', now = format_date_joined() ) 
+
+
+
+def format_date_joined():
+    now = datetime.datetime.now()
+    return ("Joined " + now.strftime("%B, %Y"))
 
 
 if __name__ == '__main__':
